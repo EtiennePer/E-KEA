6be5716275c0645f568c53442df91a0bae25c372
EtiennePer <etienne.pericat.etu@gmail.com>
2018-12-05 15:46:59 +0100

    create empty activities

---
+++ app/src/main/java/ecom/m1ice/e_kea/HomeActivity.java
@@ -0,0 +1,13 @@
+package ecom.m1ice.e_kea;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
