a85f5d4a23f5588c4314c4ee8bba1a32ab5743e4
Etienne <etienne.pericat.etu@gmail.com>
2019-02-13 14:33:45 +0100

    Basic SplashActiviy

---
+++ app/src/main/java/ecom/m1ice/e_kea/SplashActivity.java
@@ -0,0 +1,32 @@
+package ecom.m1ice.e_kea;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import ecom.m1ice.e_kea.GlobalVars;
+import ecom.m1ice.e_kea.R;
+import ecom.m1ice.e_kea.models.Etienne;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        this.setTheme(R.style.AppTheme);
+        super.onCreate(savedInstanceState);
+
+        // TODO : read TAG and select the good user
+        ((GlobalVars) this.getApplication()).setActualUser(new Etienne()); // for example
+
+
+        routeToAppropriatePage();
+        finish();
+    }
+
+    private void routeToAppropriatePage() {
+
+        Intent intent = new Intent(this, ResumeActivity.class);
+        startActivity(intent);
+    }
+}
