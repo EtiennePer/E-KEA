54a5e649061a20e2ff54177e049f0f272bf4029f
Etienne <etienne.pericat.etu@gmail.com>
2019-02-13 13:48:51 +0100

    Basic home page

--- app/src/main/java/ecom/m1ice/e_kea/ResumeActivity.java
+++ app/src/main/java/ecom/m1ice/e_kea/ResumeActivity.java
@@ -1,13 +1,47 @@
 package ecom.m1ice.e_kea;
 
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.mikhaellopez.circularimageview.CircularImageView;
+
+import ecom.m1ice.e_kea.models.User;
 
 public class ResumeActivity extends AppCompatActivity {
+
+    private TextView tv_firstLastname;
+    private CircularImageView img_pp;
+
+    private Button bt_resume;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resume);
+
+        tv_firstLastname = findViewById(R.id.RESUME_tv_FirstnameLastname);
+        img_pp = findViewById(R.id.RESUME_img_profile);
+
+        // Retrieve the actual user
+        User actualUser = ((GlobalVars) this.getApplication()).getActualUser();
+
+        // Set content of form
+        tv_firstLastname.setText(actualUser.getFirstname() + " " + actualUser.getLastname());
+
+        // Retrieve the good drawable
+        Drawable img = null;
+        if (actualUser.getFirstname().toLowerCase().contains("eti")) {
+            img = getResources().getDrawable(R.drawable.pp_etienne);
+        } else if (actualUser.getFirstname().toLowerCase().contains("ka")) {
+            img = getResources().getDrawable(R.drawable.pp_kais);
+        } else if (actualUser.getFirstname().toLowerCase().contains("alex")) {
+            img = getResources().getDrawable(R.drawable.pp_alexandre);
+        }
+
+        img_pp.setImageDrawable(img);
     }
 }
---
+++ app/src/main/java/ecom/m1ice/e_kea/models/Etienne.java
@@ -0,0 +1,36 @@
+package ecom.m1ice.e_kea.models;
+
+import java.util.Date;
+
+public class Etienne implements User {
+    @Override
+    public String getFirstname() {
+        return "Etienne";
+    }
+
+    @Override
+    public String getLastname() {
+        return "Pericat";
+    }
+
+    @Override
+    public Date getBithDate() {
+        return new Date(1997, 02, 11);
+    }
+
+    @Override
+    public String getDescription() {
+        return "Bonjour voici une descirpiot de quelques phrase sur moi. Elle est vraiment très intéressante" +
+                " n'est ce pas ? oui bonjour.";
+    }
+
+    @Override
+    public String getPhotoPathName() {
+        return "pp_etienne.png";
+    }
+
+    @Override
+    public String getResumePathName() {
+        return "cv_etienne.png";
+    }
+}
